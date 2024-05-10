facd5206daa4c4d39e564bf26c11ad1eb61c7f10
Isra-DVD <usr3849@salesianos-lacuesta.net>
2024-05-10 23:39:36 +0100

    feat(app): Implementada clase ejecutable completa

--- src/App.java
+++ src/App.java
@@ -1,5 +1,53 @@
+import javax.swing.JOptionPane;
+
+import net.salesianos.menu.Menu;
+import net.salesianos.diccionario.Diccionario;
+import net.salesianos.validaciones.Validador;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        boolean seguirPreguntando = true;
+        JOptionPane.showMessageDialog(null, "Bienvenido al super diccionario de DUOLINGO");
+
+        while (seguirPreguntando) {
+
+            String opcionUsuario = JOptionPane.showInputDialog(Menu.mostrar());
+            if (Validador.noNulo(opcionUsuario)) {
+
+                switch (opcionUsuario) {
+                    case "1":
+                        Diccionario.añadirPalabraAlSet();
+                        break;
+
+                    case "2":
+                        Diccionario.eliminarPalabra();
+                        break;
+
+                    case "3":
+                        Diccionario.buscarPalabra();
+                        break;
+
+                    case "4":
+                        Diccionario.mostrarInciales();
+                        break;
+
+                    case "5":
+                        Diccionario.mostrarPalabrasPorLetra();
+                        break;
+
+                    case "6":
+                        JOptionPane.showMessageDialog(null, "Adiós\n");
+                        seguirPreguntando = false;
+                        break;
+
+                    default:
+                        JOptionPane.showMessageDialog(null, "Valor invalido, vuelva a intentarlo\n");
+                        break;
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "Valor invalido, vuelva a intentarlo\n");
+            }
+        }
     }
 }
